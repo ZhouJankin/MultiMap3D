348e223b56504e66b477227dd737da81dc6005a5
Arturs Elksnis <arturs@elksnis.co.uk>
2023-03-08 17:49:28 +0000

    Generator now generates pickle files ready for classifier.

--- ORB-SLAM2_DENSE-master/Examples/ROS/ORB_SLAM2_DENSE/scripts/room_classifier/room_labels_and_features_generator.py
+++ ORB-SLAM2_DENSE-master/Examples/ROS/ORB_SLAM2_DENSE/scripts/room_classifier/room_labels_and_features_generator.py
@@ -1,4 +1,5 @@
 import random
+import pickle
 
 # So we have these features - that's all we know how to detect.
 # Let's construct a few labels (room types) and assign some of these items to them
@@ -33,8 +34,10 @@ possible_items_in_each_room = [
 
 training_data = []
 
+TRAINING_DATASETS_TO_GENERATE = 120
+
 # generate some data by randomizing potential objects in each room category
-for i in range(120):
+for i in range(TRAINING_DATASETS_TO_GENERATE):
     rt = random.randrange(0, len(room_types)) # choose room type to generate
     max_item_count_in_room = len(possible_items_in_each_room[rt][1]) # how many classes of items do we have in this type of room
     item_cnt_to_generate = random.randrange(1, max_item_count_in_room + 1) # how many items we will generate for this room
@@ -42,6 +45,30 @@ for i in range(120):
     item_indexes = random.sample(range(0, max_item_count_in_room), item_cnt_to_generate) # the indexes of the generated items
     training_data.append((room_types[rt], [possible_items_in_each_room[rt][1][idx] for idx in item_indexes]))
     
-    print((room_types[rt], [possible_items_in_each_room[rt][1][idx] for idx in item_indexes]))
+    #print((room_types[rt], [possible_items_in_each_room[rt][1][idx] for idx in item_indexes]))
+
+# Now store everything in pickle files
+labels_fname = "labels_shuffled.pkl"
+features_fname = "features_for_each_label.pkl"
+
+labels_shuffled = []
+features_for_each_label = []
+#labels_shuffled = [room_data[0] for room_data in training_data]
+
+for room_data in training_data:
+    labels_shuffled.append(room_data[0])
+    # now we'll get the objects into a string separated by a space
+    objs_in_room_as_string = ""
+    for obj in room_data[1]:
+        objs_in_room_as_string += obj + " "
+    features_for_each_label.append(objs_in_room_as_string[:-1])
+
+pickle.dump(labels_shuffled, open(labels_fname, "wb"))
+pickle.dump(features_for_each_label, open(features_fname, "wb"))
+
+#print(labels_shuffled)
+#print(features_for_each_label)
+
+#pickle.dump(training_data, open("training_data.pkl", "wb"))
 
 #print(training_data)
