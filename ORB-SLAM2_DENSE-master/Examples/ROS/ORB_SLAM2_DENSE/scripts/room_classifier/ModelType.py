fc440bd033a88da53bb0db0ce2541e6b3e6acf78
Arturs Elksnis <arturs@elksnis.co.uk>
2023-03-11 15:42:03 +0000

    Updated comments for the different detection engines.

--- ORB-SLAM2_DENSE-master/Examples/ROS/ORB_SLAM2_DENSE/scripts/room_classifier/ModelType.py
+++ ORB-SLAM2_DENSE-master/Examples/ROS/ORB_SLAM2_DENSE/scripts/room_classifier/ModelType.py
@@ -6,8 +6,8 @@ from enum import Enum
 # generate the samples for. This enum will help with that.
 ###
 class ModelType(Enum):
-    FEATURES_12 = 12 # for the model with 12 detectable classes # Turns out that FEATURES_12 detector a.k.a. sunrgbd class actually only provides 10 classes, not 12, but I'm not changing this anymore because it's referenced in many places in code.
-    FEATURES_18 = 18 # for the model with 18 detectable classes
-    AI2_THOR = 100
-    AI2_THOR_12 = 112 # for AI2-THOR training data with classes filtered out to only contain the 12 from the 12-sample detection engine
-    AI2_THOR_18 = 118 # for AI2-THOR training data with classes filtered out to only contain the 18 from the 18-sample detection engine
+    FEATURES_12 = 12 # For sunrgbd detector (10 features) - generated rooms and features from available data. # Turns out that FEATURES_12 detector a.k.a. sunrgbd class actually only provides 10 classes, not 12, but I'm not changing this anymore because it's referenced in many places in code.
+    FEATURES_18 = 18 # For scannet detector (18 features) - generated rooms and features from available data.    
+    AI2_THOR = 100 # For the original AI2-THOR data extracted from the simulation
+    AI2_THOR_12 = 112 # For sunrgbd data filtered and translated to AI2-THOR vocabulary and then AI2-THOR data used with sunrgbd features
+    AI2_THOR_18 = 118 # For scannet data filtered and translated to AI2-THOR vocabulary and then AI2-THOR data used with scannet features
